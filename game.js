316efc4bf27ff936fef9a7e4334a9d45f5d54a72
Lucas <dexterfail@hotmail.com>
2021-08-02 16:23:10 -0300

    salve

--- game.js
+++ game.js
@@ -10,51 +10,51 @@ const MOVE_SPEED = 200
 
 // Error with import images
 
-// loadRoot("./assets/sprites/");
-// loadSprite("link-left", "/link-left.png");
-// loadSprite("link-right", "/link-right.png");
-// loadSprite("link-down", "/link-down.png");
-// loadSprite("link-up", "/link-up.png");
-// loadSprite("left-wall", "/leftwall.png");
-// loadSprite("top-wall", "/top-wall.png");
-// loadSprite("bottom-wall", "/bottom-wall.png");
-// loadSprite("right-wall", "/right-wall.png");
-// loadSprite("bottom-left-wall", "/bottom-left-wall.png");
-// loadSprite("bottom-right-wall", "/bottom-right-wall.png");
-// loadSprite("top-left-wall", "/top-left-wall.png");
-// loadSprite("top-right-wall", "/top-rigth-wall.jpg");
-// loadSprite("top-door", "/top-door.png");
-// loadSprite("fire-pot", "/fire-pot.png");
-// loadSprite("left-door", "/left-door.png");
-// loadSprite("lanterns", "/lanterns.png");
-// loadSprite("slicer", "/slicer.png");
-// loadSprite("skeletor", "/skeletor.png");
-// loadSprite("kaboom", "/kaboom.png");
-// loadSprite("stairs", "/stairs.png");
-// loadSprite("bg", "/bg.png");
+loadRoot("sprites/");
+loadSprite("link-left", "link-left.png");
+loadSprite("link-right", "link-right.png");
+loadSprite("link-down", "link-down.png");
+loadSprite("link-up", "link-up.png");
+loadSprite("left-wall", "left-wall.png");
+loadSprite("top-wall", "top-wall.png");
+loadSprite("bottom-wall", "bottom-wall.png");
+loadSprite("right-wall", "right-wall.png");
+loadSprite("bottom-left-wall", "bottom-left-wall.png");
+loadSprite("bottom-right-wall", "bottom-right-wall.png");
+loadSprite("top-left-wall", "top-left-wall.png");
+loadSprite("top-right-wall", "top-right-wall.jpg");
+loadSprite("top-door", "top-door.png");
+loadSprite("fire-pot", "fire-pot.png");
+loadSprite("left-door", "left-door.png");
+loadSprite("lanterns", "lanterns.png");
+loadSprite("slicer", "slicer.png");
+loadSprite("skeletor", "skeletor.png");
+loadSprite("kaboom", "kaboom.png");
+loadSprite("stairs", "stairs.png");
+loadSprite("bg", "bg.png");
 
-loadRoot('https://i.imgur.com/')
-loadSprite('link-left', '1Xq9biB.png')
-loadSprite('link-right', 'yZIb8O2.png')
-loadSprite('link-down', 'tVtlP6y.png')
-loadSprite('link-up', 'UkV0we0.png')
-loadSprite('left-wall', 'rfDoaa1.png')
-loadSprite('top-wall', 'QA257Bj.png')
-loadSprite('bottom-wall', 'vWJWmvb.png')
-loadSprite('right-wall', 'SmHhgUn.png')
-loadSprite('bottom-left-wall', 'awnTfNC.png')
-loadSprite('bottom-right-wall', '84oyTFy.png')
-loadSprite('top-left-wall', 'xlpUxIm.png')
-loadSprite('top-right-wall', 'z0OmBd1.jpg')
-loadSprite('top-door', 'U9nre4n.png')
-loadSprite('fire-pot', 'I7xSp7w.png')
-loadSprite('left-door', 'okdJNls.png')
-loadSprite('lanterns', 'wiSiY09.png')
-loadSprite('slicer', 'c6JFi5Z.png')
-loadSprite('skeletor', 'Ei1VnX8.png')
-loadSprite('kaboom', 'o9WizfI.png')
-loadSprite('stairs', 'VghkL08.png')
-loadSprite('bg', 'u4DVsx6.png')
+// loadRoot('https://i.imgur.com/')
+// loadSprite('link-left', '1Xq9biB.png')
+// loadSprite('link-right', 'yZIb8O2.png')
+// loadSprite('link-down', 'tVtlP6y.png')
+// loadSprite('link-up', 'UkV0we0.png')
+// loadSprite('left-wall', 'rfDoaa1.png')
+// loadSprite('top-wall', 'QA257Bj.png')
+// loadSprite('bottom-wall', 'vWJWmvb.png')
+// loadSprite('right-wall', 'SmHhgUn.png')
+// loadSprite('bottom-left-wall', 'awnTfNC.png')
+// loadSprite('bottom-right-wall', '84oyTFy.png')
+// loadSprite('top-left-wall', 'xlpUxIm.png')
+// loadSprite('top-right-wall', 'z0OmBd1.jpg')
+// loadSprite('top-door', 'U9nre4n.png')
+// loadSprite('fire-pot', 'I7xSp7w.png')
+// loadSprite('left-door', 'okdJNls.png')
+// loadSprite('lanterns', 'wiSiY09.png')
+// loadSprite('slicer', 'c6JFi5Z.png')
+// loadSprite('skeletor', 'Ei1VnX8.png')
+// loadSprite('kaboom', 'o9WizfI.png')
+// loadSprite('stairs', 'VghkL08.png')
+// loadSprite('bg', 'u4DVsx6.png')
 
 scene("game", ({level,score}) => {
     layers(['bg','obj','ui'], 'obj')
